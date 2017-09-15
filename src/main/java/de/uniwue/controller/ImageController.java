5e064edc44d28fbe837c9973cd1c5b88e4b72344
Alexander Hartelt <alexander.hartelt@stud-mail.uni-wuerzburg.de>
2017-09-15 14:53:03 +0200

    Code cleanup

--- src/main/java/de/uniwue/controller/ImageController.java
+++ src/main/java/de/uniwue/controller/ImageController.java
@@ -15,63 +15,72 @@ import de.uniwue.helper.ImageHelper;
 
 @Controller
 public class ImageController {
-    @RequestMapping(value = "/ajax/image/page" , method = RequestMethod.GET)
-    public @ResponseBody String getImageOfPage(
-                @RequestParam("pageId") String pageId,
-                @RequestParam("imageId") String imageId,
-                HttpSession session, HttpServletResponse response
-            ) throws IOException {
-        String projectDir = (String)session.getAttribute("projectDir");
-        // To trigger AJAX fail (and therefore show errors)
-        if (projectDir == null || projectDir.isEmpty() || pageId == null || pageId.isEmpty() || imageId == null || imageId.isEmpty()) {
+    @RequestMapping(value = "/ajax/image/page", method = RequestMethod.GET)
+    public @ResponseBody String getImageOfPage(@RequestParam("pageId") String pageId,
+            @RequestParam("imageId") String imageId, HttpSession session, HttpServletResponse response)
+            throws IOException {
+        String projectDir = (String) session.getAttribute("projectDir");
+        // Error hadnling to trigger AJAX fail (and therefore show errors)
+        if (projectDir == null || projectDir.isEmpty() || pageId == null || pageId.isEmpty() || imageId == null
+                || imageId.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        ImageHelper imageHelper = new ImageHelper();
-        String image64 = imageHelper.getPageImage(projectDir, pageId, imageId);
-        if (image64 == null) {
+        String image64 = null;
+        try {
+            ImageHelper imageHelper = new ImageHelper();
+            image64 = imageHelper.getPageImage(projectDir, pageId, imageId);
+        } catch (IOException e) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
+        if (image64 == null)
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
         return image64;
     }
 
-    @RequestMapping(value = "/ajax/image/segment" , method = RequestMethod.GET)
-    public @ResponseBody String getImageOfSegment(
-                @RequestParam("pageId") String pageId,
-                @RequestParam("imageId") String imageId,
-                HttpSession session, HttpServletResponse response
-            ) throws IOException {
-        String projectDir = (String)session.getAttribute("projectDir");
+    @RequestMapping(value = "/ajax/image/segment", method = RequestMethod.GET)
+    public @ResponseBody String getImageOfSegment(@RequestParam("pageId") String pageId,
+            @RequestParam("imageId") String imageId, HttpSession session, HttpServletResponse response)
+            throws IOException {
+        String projectDir = (String) session.getAttribute("projectDir");
         String imageType = session.getAttribute("imageType").toString();
-        if (projectDir == null || projectDir.isEmpty() || imageType == null || imageType.isEmpty() || pageId == null || pageId.isEmpty() || imageId == null || imageId.isEmpty()) {
+        if (projectDir == null || projectDir.isEmpty() || imageType == null || imageType.isEmpty() || pageId == null
+                || pageId.isEmpty() || imageId == null || imageId.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        ImageHelper imageHelper = new ImageHelper();
-        String image64 = imageHelper.getSegmentImage(projectDir, pageId, imageId, imageType);
-        // To trigger AJAX fail (and therefore show errors)
-        if (image64 == null) {
+        String image64 = null;
+        try {
+            ImageHelper imageHelper = new ImageHelper();
+            image64 = imageHelper.getSegmentImage(projectDir, pageId, imageId, imageType);
+        } catch (IOException e) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
+        if (image64 == null)
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
         return image64;
     }
 
-    @RequestMapping(value = "/ajax/image/line" , method = RequestMethod.GET)
-    public @ResponseBody String getImageOfLine(
-                @RequestParam("pageId") String pageId,
-                @RequestParam("segmentId") String segmentId,
-                @RequestParam("imageId") String imageId,
-                HttpSession session, HttpServletResponse response
-            ) throws IOException {
-        String projectDir = (String)session.getAttribute("projectDir");
+    @RequestMapping(value = "/ajax/image/line", method = RequestMethod.GET)
+    public @ResponseBody String getImageOfLine(@RequestParam("pageId") String pageId,
+            @RequestParam("segmentId") String segmentId, @RequestParam("imageId") String imageId, HttpSession session,
+            HttpServletResponse response) throws IOException {
+        String projectDir = (String) session.getAttribute("projectDir");
         String imageType = session.getAttribute("imageType").toString();
-        if (projectDir == null || projectDir.isEmpty() || imageType == null || imageType.isEmpty() || pageId == null || pageId.isEmpty() || segmentId ==null || segmentId.isEmpty() || imageId == null || imageId.isEmpty()) {
+        if (projectDir == null || projectDir.isEmpty() || imageType == null || imageType.isEmpty() || pageId == null
+                || pageId.isEmpty() || segmentId == null || segmentId.isEmpty() || imageId == null
+                || imageId.isEmpty()) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-        ImageHelper imageHelper = new ImageHelper();
-        String image64 = imageHelper.getLineImage(projectDir, pageId, segmentId, imageId, imageType);
-        // To trigger AJAX fail (and therefore show errors)
-        if (image64 == null) {
+        String image64 = null;
+        try {
+            ImageHelper imageHelper = new ImageHelper();
+            image64 = imageHelper.getLineImage(projectDir, pageId, segmentId, imageId, imageType);
+        } catch (IOException e) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }
+        if (image64 == null)
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
         return image64;
     }
--- src/main/java/de/uniwue/helper/ImageHelper.java
+++ src/main/java/de/uniwue/helper/ImageHelper.java
@@ -2,81 +2,77 @@ package de.uniwue.helper;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Base64;
 
 public class ImageHelper {
 
     public ImageHelper() {
-        
     }
-    public String encodeFileToBase64Binary(File file){
+
+    public String encodeFileToBase64Binary(File file) throws IOException {
         String encodedfile = null;
-        try {
-            FileInputStream fileInputStreamReader = new FileInputStream(file);
-            byte[] bytes = new byte[(int)file.length()];
-            fileInputStreamReader.read(bytes);
-            encodedfile = Base64.getEncoder().encodeToString(bytes);
-            fileInputStreamReader.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        FileInputStream fileInputStreamReader = new FileInputStream(file);
+        byte[] bytes = new byte[(int) file.length()];
+        fileInputStreamReader.read(bytes);
+        encodedfile = Base64.getEncoder().encodeToString(bytes);
+        fileInputStreamReader.close();
         return encodedfile;
     }
-    public String getPageImage(String projectDir, String pageID, String imageID) {
+
+    public String getPageImage(String projectDir, String pageID, String imageID) throws IOException {
         String base64Image = null;
         File f = null;
-        if(imageID.equals("Original")) {
-            f =  new File(projectDir + File.separator + "Original" + File.separator + pageID+".png");
-        }
-        else {
-            if(imageID.equals("Gray")) {
-                f = new File(projectDir + File.separator + "PreProc" + File.separator
-                        + "Gray" + File.separator + pageID+".png");
+        if (imageID.equals("Original")) {
+            f = new File(projectDir + File.separator + "Original" + File.separator + pageID + ".png");
+        } else {
+            if (imageID.equals("Gray")) {
+                f = new File(projectDir + File.separator + "PreProc" + File.separator + "Gray" + File.separator + pageID
+                        + ".png");
             }
-            if(imageID.equals("Binary")) {
-                f = new File(projectDir + File.separator + "PreProc" + File.separator
-                        + "Binary" + File.separator + pageID+".png");
+            if (imageID.equals("Binary")) {
+                f = new File(projectDir + File.separator + "PreProc" + File.separator + "Binary" + File.separator
+                        + pageID + ".png");
             }
-            if(imageID.equals("Despeckled")) {
-                f = new File(projectDir + File.separator + "PreProc" + File.separator
-                        + "Despeckled" + File.separator + pageID+".png");
+            if (imageID.equals("Despeckled")) {
+                f = new File(projectDir + File.separator + "PreProc" + File.separator + "Despeckled" + File.separator
+                        + pageID + ".png");
             }
         }
         if (f.exists())
-            base64Image=encodeFileToBase64Binary(f);
+            base64Image = encodeFileToBase64Binary(f);
 
         return base64Image;
     }
 
-    public String getSegmentImage(String projectDir, String pageID, String segmentID, String imageType) {
+    public String getSegmentImage(String projectDir, String pageID, String segmentID, String imageType)
+            throws IOException {
         String base64Image = null;
         File f = null;
-        if(imageType.equals("Gray")) 
-            f = new File(projectDir+File.separator+"OCR"+File.separator+"Pages"+File.separator+pageID+File.separator+segmentID+".nrm.png");
-        if(imageType.equals("Binary")) 
-            f = new File(projectDir+File.separator+"OCR"+File.separator+"Pages"+File.separator+pageID+File.separator+segmentID+".bin.png");
+        if (imageType.equals("Gray"))
+            f = new File(projectDir + File.separator + "OCR" + File.separator + "Pages" + File.separator + pageID
+                    + File.separator + segmentID + ".nrm.png");
+        if (imageType.equals("Binary"))
+            f = new File(projectDir + File.separator + "OCR" + File.separator + "Pages" + File.separator + pageID
+                    + File.separator + segmentID + ".bin.png");
         if (f.exists())
-            base64Image=encodeFileToBase64Binary(f);
+            base64Image = encodeFileToBase64Binary(f);
 
         return base64Image;
     }
 
-    public String getLineImage(String projectDir, String pageID, String segmentID, String lineID, String imageType) {
+    public String getLineImage(String projectDir, String pageID, String segmentID, String lineID, String imageType)
+            throws IOException {
         String base64Image = null;
         File f = null;
-        if(imageType.equals("Gray")) 
-            f = new File(projectDir+File.separator+"OCR"+File.separator+"Pages"+File.separator+pageID+File.separator+segmentID+File.separator+lineID+".nrm.png");
-        if(imageType.equals("Binary")) 
-            f = new File(projectDir+File.separator+"OCR"+File.separator+"Pages"+File.separator+pageID+File.separator+segmentID+File.separator+lineID+".bin.png");
+        if (imageType.equals("Gray"))
+            f = new File(projectDir + File.separator + "OCR" + File.separator + "Pages" + File.separator + pageID
+                    + File.separator + segmentID + File.separator + lineID + ".nrm.png");
+        if (imageType.equals("Binary"))
+            f = new File(projectDir + File.separator + "OCR" + File.separator + "Pages" + File.separator + pageID
+                    + File.separator + segmentID + File.separator + lineID + ".bin.png");
         if (f.exists())
-            base64Image=encodeFileToBase64Binary(f);
-
+            base64Image = encodeFileToBase64Binary(f);
         return base64Image;
     }
 
