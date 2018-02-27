fce427f44a72482ba955f0fee12ac472b2856b39
Dennis Christ <dennis.christ@stud-mail.uni-wuerzburg.de>
2018-02-27 17:32:14 +0100

    Added first part of segmentation module
    - TODO: Implement copy process (to OCR folder) for PageXML files

---
+++ src/main/java/de/uniwue/controller/Segmentation.java
@@ -0,0 +1,23 @@
+package de.uniwue.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Segmentation {
+    @RequestMapping("/Segmentation")
+    public ModelAndView showPreprocessing(HttpSession session) {
+        ModelAndView mv = new ModelAndView("segmentation");
+
+        String projectDir = (String)session.getAttribute("projectDir");
+        if (projectDir == null) {
+            mv.addObject("error", "Session expired.\nPlease return to the Project Overview page.");
+            return mv;
+        }
+
+        return mv;
+    }
+}
