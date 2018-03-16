993f370f7ad0fcdc0dd59210c7a2cf10c3dbae75
Dennis Christ <dennis.christ@stud-mail.uni-wuerzburg.de>
2018-03-16 19:18:18 +0100

    Removed unnecessary import in Preprocessing helper

--- src/main/java/de/uniwue/helper/PreprocessingHelper.java
+++ src/main/java/de/uniwue/helper/PreprocessingHelper.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import de.uniwue.config.ProjectConfiguration;
