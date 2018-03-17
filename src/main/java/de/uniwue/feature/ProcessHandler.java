d1794523a7a42ddf8b81bfd609a5d5ae7c17d97e
Alexander Hartelt <alexander.hartelt@stud-mail.uni-wuerzburg.de>
2018-03-17 16:48:42 +0100

    Changed coding to UTF8 in processhandler

--- src/main/java/de/uniwue/feature/ProcessHandler.java
+++ src/main/java/de/uniwue/feature/ProcessHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -39,9 +40,9 @@ public class ProcessHandler {
 
         @Override
         public void run() {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String nextLine = null;
             try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String nextLine = null;
                 while ((nextLine = reader.readLine()) != null) {
                     consumeInputLine.accept(nextLine);
                 }
