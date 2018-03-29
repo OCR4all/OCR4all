b135b9d0d1a044fbbd812d18964d2385ce663825
Dennis Christ <dennis.christ@stud-mail.uni-wuerzburg.de>
2018-03-29 14:58:44 +0200

    Removed unnecessary import in ResultController

--- src/main/java/de/uniwue/controller/ResultController.java
+++ src/main/java/de/uniwue/controller/ResultController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import de.uniwue.helper.RecognitionHelper;
 import de.uniwue.helper.ResultHelper;
 
 /**
