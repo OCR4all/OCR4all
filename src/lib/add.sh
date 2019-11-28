8a084142b1eb8ddae6f98599cf739a07935b1ab0
Kevin Chadbourne <51399945+chaddy314@users.noreply.github.com>
2019-11-28 15:50:48 +0100

    Delete add.sh

--- src/lib/add.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILE=$1
-GROUPID=$2
-ARTIFACTID=$3
-VERSION=$4
-
-mvn install:install-file -Dfile=$FILE -DgroupId=$GROUPID -DartifactId=$ARTIFACTID -Dversion=$VERSION -Dpackaging=jar -DlocalRepositoryPath=repository/
